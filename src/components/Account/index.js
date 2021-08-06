4216f38292d391661562d3ddc72a22cac86996f0
Jaymin Patel <jaymin.future365@gmail.com>
2021-08-06 12:52:27 +0530

    Used cotext to show user email in Account page

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -3,14 +3,17 @@ import * as React from 'react';
 import {PasswordForgetForm} from "../PasswordForget";
 import PasswordChangeForm from "../PasswordChange";
 import { withAuthorization } from '../Session'
+import AuthUserContext from "../Session/context";
 
-const Account = () => (
+const Account = () => {
+    const authUser = React.useContext(AuthUserContext);
+    return (
     <div>
-        <h1>Account Page</h1>
+        <h1>Account: {authUser.email}</h1>
         <PasswordForgetForm/>
         <PasswordChangeForm/>
     </div>
-);
+)};
 
 const condition = authUser => !!authUser;
 
